bf85e7863cfc1c73af67ab2da6bf5f3211b7ce5d
Dante Garcia <dantegarciaram93@gmail.com>
2024-10-21 10:34:51 -0700

    Initial Commit

---
+++ GrowTakeHome/GrowTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrowTakeHomeApp.swift
+//  GrowTakeHome
+//
+//  Created by Dante Garcia on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct GrowTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
